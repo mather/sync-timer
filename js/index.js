ae5204a6702c5faf7458189a9093e11dcb47e18b
mather <mather314@gmail.com>
2022-12-03 16:20:11 +0900

    Minimal names in global scope.
    グローバルスコープになるべく名前を作らない。

--- js/index.js
+++ js/index.js
@@ -6,31 +6,33 @@ import "@fortawesome/fontawesome-free/css/brands.css";
 import "@fortawesome/fontawesome-free/css/solid.css";
 import "../css/main.scss";
 
-const searchParams = (new URL(document.location)).searchParams;
-const parseFg = (s) => {
-  if (!s) return null;
-  const re = /^#[0-9a-f]{6}$/;
-  if (re.test(s)) return s;
-  return null;
-}
-const parseInit = (s) => {
-  if (s) {
-    const n = parseInt(s, 10);
-    if (n > 30 || n < -30) return null;
-    return n;
+const parseParams = () => {
+  const searchParams = (new URL(document.location)).searchParams;
+  const parseFg = (s) => {
+    if (!s) return null;
+    const re = /^#[0-9a-f]{6}$/;
+    if (re.test(s)) return s;
+    return null;
   }
-  return null;
-}
-const flags = {
-  fg: parseFg(searchParams.get("fg")),
-  bg: searchParams.get("bg"),
-  init: parseInit(searchParams.get("init")),
-  h: searchParams.get("h")
+  const parseInit = (s) => {
+    if (s) {
+      const n = parseInt(s, 10);
+      if (n > 30 || n < -30) return null;
+      return n;
+    }
+    return null;
+  }
+  return {
+    fg: parseFg(searchParams.get("fg")),
+    bg: searchParams.get("bg"),
+    init: parseInit(searchParams.get("init")),
+    h: searchParams.get("h")
+  };
 };
 
 const app = Elm.Main.init({
   node: document.getElementById("root"),
-  flags: flags
+  flags: parseParams()
 });
 
 app.ports.setQueryString.subscribe((newQS) => {
