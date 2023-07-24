4a9e80d78089d9d45c5cd999e5ab9fa0349ac97f
mather <mather314@gmail.com>
2023-07-24 18:11:37 +0900

    Add rollupOptions to build en.html

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,15 @@
-import { defineConfig } from 'vite'
-import elmPlugin from 'vite-plugin-elm'
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import elmPlugin from 'vite-plugin-elm';
 
 export default defineConfig({
-  plugins: [elmPlugin()]
+  plugins: [elmPlugin()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve("index.html"),
+        en: resolve("en.html")
+      }
+    }
+  }
 })
