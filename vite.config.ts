d9bacc47690f79c6c852adcf78501f1ac33f726b
mather <mather314@gmail.com>
2023-07-30 02:57:40 +0900

    Use vite-plugin-sitemap to generate sitemap.xml

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import elmPlugin from 'vite-plugin-elm';
+import Sitemap from "vite-plugin-sitemap";
+
+const sitemapConfig = {
+  hostname: "https://sync-timer.netlify.app/",
+}
 
 export default defineConfig({
-  plugins: [elmPlugin()],
+  plugins: [elmPlugin(), Sitemap(sitemapConfig)],
   build: {
     rollupOptions: {
       input: {
